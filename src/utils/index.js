72f74b25710bc4869d4688b4b69dc031458595e0
Shahriar <bidyuthasan9@gmail.com>
2024-09-24 11:53:40 +0600

    first commit

---
+++ src/utils/index.js
@@ -0,0 +1,28 @@
+export const heroImg = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155660/hero_irv0zp.jpg';
+
+export const heroVideo = 'https://res.cloudinary.com/dlgvj2apk/video/upload/v1727155671/hero_mjhqba.mp4';
+export const smallHeroVideo = 'https://res.cloudinary.com/dlgvj2apk/video/upload/v1727155675/smallHero_vkvpui.mp4';
+export const highlightFirstVideo = 'https://res.cloudinary.com/dlgvj2apk/video/upload/v1727155674/highlight-first_pbwczc.mp4';
+export const highlightSecondVideo = 'https://res.cloudinary.com/dlgvj2apk/video/upload/v1727155676/hightlight-third_qdjdrt.mp4';
+export const highlightThirdVideo = 'https://res.cloudinary.com/dlgvj2apk/video/upload/v1727155672/hightlight-sec_zph13n.mp4';
+export const highlightFourthVideo = 'https://res.cloudinary.com/dlgvj2apk/video/upload/v1727155675/hightlight-fourth_xxabvg.mp4';
+export const exploreVideo = 'https://res.cloudinary.com/dlgvj2apk/video/upload/v1727155671/explore_qlj4fg.mp4';
+export const frameVideo = 'https://res.cloudinary.com/dlgvj2apk/video/upload/v1727155672/frame_katjwl.mp4';
+
+export const appleImg = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155655/apple_xl5kjh.svg';
+export const searchImg = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155664/search_oyra05.svg';
+export const bagImg = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155655/bag_wwdcqk.svg';
+export const watchImg = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155665/watch_rb6vfp.svg';
+export const rightImg = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155663/right_ax7elr.svg';
+export const replayImg = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155663/replay_a27esa.svg';
+export const playImg = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155662/play_wkbjml.svg';
+export const pauseImg = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155661/pause_crdd9p.svg';
+
+export const yellowImg = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155668/yellow_zrtm0s.jpg';
+export const blueImg = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155660/blue_aledkm.jpg';
+export const whiteImg = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155667/white_ryrg8g.jpg';
+export const blackImg = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155659/black_v99t19.jpg';
+export const explore1Img = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155658/explore1_ixpmiz.jpg';
+export const explore2Img = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155660/explore2_lhecae.jpg';
+export const chipImg = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155657/chip_dqbwc2.jpg';
+export const frameImg = 'https://res.cloudinary.com/dlgvj2apk/image/upload/v1727155659/frame_btli7q.png';
